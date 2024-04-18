2d674ad8f9e05b90092c402b2c6447b3ea895bf6
IchaSefty <seftyexsyarii28@gmail.com>
2024-04-18 22:25:54 +0700

    Add project files.

---
+++ Tugas Praktikum PBO Abstract/Karakter.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Praktikum_PBO_Abstract
+{
+
+    public interface ISkill
+    {
+        void GunakanSkill(Karakter pengguna, Karakter target);
+        string GetDeskripsi();
+    }
+    public abstract class Karakter
+    {
+        public string Nama { get; set; }
+        public int HP { get; set; }
+        public int MP { get; set; }
+        public int Kekuatan { get; set; }
+
+        public Karakter(string nama, int hp, int mp, int kekuatan)
+        {
+            Nama = nama;
+            HP = hp;
+            MP = mp;
+            Kekuatan = kekuatan;
+        }
+
+        public virtual void Serang(Karakter target)
+        {
+            Console.WriteLine($"{Nama} menyerang {target.Nama}!");
+            target.Serang(this);
+        }
+        public abstract void GunakanSkill(ISkill skill, Karakter target);
+
+        public virtual void CetakInformasi()
+        {
+            Console.WriteLine($"Nama: {Nama}, HP: {HP}, MP: {MP}, Kekuatan: {Kekuatan}");
+        }
+
+        public bool ApakahHidup()
+        {
+            return HP > 0;
+        }
+    }
+    public class Hero : Karakter
+    {
+      
+        public Hero(string nama, int hp, int mp, int kekuatan) : base(nama, hp, mp, kekuatan)
+        {
+        }
+
+        public override void GunakanSkill(ISkill skill, Karakter target)
+        {
+            skill.GunakanSkill(this, target);
+        }
+    }
+
+    public class Musuh : Karakter
+    {
+        public Musuh(string nama, int hp, int kekuatan) : base(nama, hp, 0, kekuatan)
+        {
+        }
+
+ 
+        public override void Serang(Karakter karakter)
+        {
+            HP -= karakter.Kekuatan;
+            Console.WriteLine($"{Nama} menerima serangan dari {karakter.Nama}!");
+            if (!ApakahHidup())
+                Mati();
+        }
+
+        public void Mati()
+        {
+            Console.WriteLine($"{Nama} telah mati.");
+        }
+
+       
+        public override void GunakanSkill(ISkill skill, Karakter target)
+        {
+            Console.WriteLine($"{Nama} tidak dapat menggunakan skill.");
+        }
+    }
+
+    public class HealSkill : ISkill
+    {
+        public void GunakanSkill(Karakter pengguna, Karakter target)
+        {
+            pengguna.HP += 25; 
+            Console.WriteLine($"{pengguna.Nama} menggunakan skill Heal, HP bertambah menjadi {pengguna.HP}");
+        }
+
+        public string GetDeskripsi()
+        {
+            return "Heal: Memulihkan HP karakter.";
+        }
+    }
+
+    
+    public class FireballSkill : ISkill
+    {
+        public void GunakanSkill(Karakter pengguna, Karakter target)
+        {
+            target.HP -= 30; 
+            Console.WriteLine($"Fireball dari {pengguna.Nama} mengenai {target.Nama}, HP berkurang menjadi {target.HP}");
+        }
+
+        public string GetDeskripsi()
+        {
+            return "Fireball: Menyerang musuh dengan damage api.";
+        }
+    }
+
+    public class IceBlastSkill : ISkill
+    {
+        public void GunakanSkill(Karakter pengguna, Karakter target)
+        {
+            target.HP -= 25; 
+            Console.WriteLine($"Ice Blast dari {pengguna.Nama} mengenai {target.Nama}, HP berkurang menjadi {target.HP}");
+            Console.WriteLine($"{target.Nama} terkena efek memperlambat.");
+        }
+
+        public string GetDeskripsi()
+        {
+            return "Ice Blast: Menyerang musuh dengan damage es dan memperlambat mereka.";
+        }
+    }
+}
---
+++ Tugas Praktikum PBO Abstract/Program.cs
@@ -0,0 +1,80 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Console.WriteLine("Hello, World!");*/
+
+namespace Tugas_Praktikum_PBO_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Game RPG");
+            Console.WriteLine("----------------------------------");
+
+
+            Hero Fanny = new Hero("Fanny", 100, 50, 20);
+
+            Musuh musuh = new Musuh("Musuh", 100, 20);
+
+            
+            ISkill heal = new HealSkill();
+            ISkill fireball = new FireballSkill();
+            ISkill iceBlast = new IceBlastSkill();
+
+            
+            Console.WriteLine("\nInformasi Karakter:");
+            Fanny.CetakInformasi();
+            Console.WriteLine("\nInformasi Musuh   :");
+            musuh.CetakInformasi();
+
+            Console.WriteLine("\n--- Mulai Pertarungan ---\n");
+
+            // Delay untuk efek dramatis
+            Thread.Sleep(2000);
+
+            while (Fanny.ApakahHidup() && musuh.ApakahHidup())
+            {
+               
+                Console.WriteLine("Pilih skill yang ingin digunakan:");
+                Console.WriteLine($"1. {heal.GetDeskripsi()}");
+                Console.WriteLine($"2. {fireball.GetDeskripsi()}");
+                Console.WriteLine($"3. {iceBlast.GetDeskripsi()}");
+
+                int pilihanSkill = int.Parse(Console.ReadLine());
+
+              
+                switch (pilihanSkill)
+                {
+                    case 1:
+                        Fanny.GunakanSkill(heal, musuh);
+                        break;
+                    case 2:
+                        Fanny.GunakanSkill(fireball, musuh);
+                        break;
+                    case 3:
+                        Fanny.GunakanSkill(iceBlast, musuh);
+                        break;
+                    default:
+                        Console.WriteLine("Pilihan tidak valid.");
+                        break;
+                }
+
+
+                if (musuh.ApakahHidup())
+                    musuh.Serang(Fanny);
+
+
+                Console.WriteLine("\nInformasi setelah menggunakan skill dan serangan musuh:");
+                Fanny.CetakInformasi();
+                musuh.CetakInformasi();
+            }
+
+            if (Fanny.ApakahHidup())
+                Console.WriteLine($"{Fanny.Nama} menang!");
+            else
+                Console.WriteLine($"{musuh.Nama} menang!");
+        }
+    }
+
+
+}
+
